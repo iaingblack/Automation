569cc7bded5627cf87f1704890c0021deb086465
Iain Black <iaingblack@gmail.com>
2017-06-23 16:33:52 +0100

    Made nice

--- Docker/Windows/cleanup-scripts/clean-all.ps1
+++ Docker/Windows/cleanup-scripts/clean-all.ps1
@@ -1,6 +1,19 @@
+cls
+Write-Host "================================================================================"
+Write-Host "SHOWING EXISTING CONTAINERS AND IMAGES"
+Write-Host "--------------------------------------"
+docker ps -a
+docker images
+Write-Host "______________________________________"
 Write-Host "Stopping Containers"
 docker ps -a -q | %{docker stop $_}
 Write-Host "Removing Containers"
 docker ps -a -q | %{docker rm -f $_}
 Write-Host "Deleting Images"
 docker images --filter dangling=true -q | %{docker rmi -f $_}
+Write-Host "_______________________________________"
+Write-Host "SHOWING REMAINING CONTAINERS AND IMAGES"
+Write-Host "--------------------------------------"
+docker ps -a
+docker images
+Write-Host "================================================================================"
