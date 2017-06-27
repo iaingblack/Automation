cc3dd0a2f3fd602fa2f7a7567a8fda7b523f8d29
Iain Black <iaingblack@gmail.com>
2017-06-27 06:36:15 +0100

    Added vscode extensions

--- ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
+++ ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
@@ -1,8 +1,18 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install vagrant chefdk virtualbox visualstudiocode 7zip -y
+choco install vagrant chefdk virtualbox visualstudiocode 7zip googlechrome notepadplusplus.install firefox -y
 refreshenv
 vagrant plugin install vagrant-winrm
 chef gem install kitchen-all
 vagrant box add mwrock/Windows2016
 vagrant box add mwrock/Windows2012R2
 vagrant box add jacqinthebox/windowsserver2016core
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension ms-vscode.powershell
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension msazurermtools.azurerm-vscode-tools
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension HookyQR.beautify
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension Pendrica.chef
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension ms-vscode.PeterJausovec.vscode-docker
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension alefragnani.project-manager
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension Borke.puppet
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension mauve.terraform
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --install-extension bbenoist.vagrant
+"C:\Program Files (x86)\Microsoft VS Code\code.exe" --list-extensions
