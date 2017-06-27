0670fb527ec08ed3c0dd50d72df85fe601b2745c
Iain Black <iaingblack@gmail.com>
2017-06-27 06:45:39 +0100

    Added git and tortoisegit

--- ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
+++ ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
@@ -1,12 +1,11 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install vagrant chefdk virtualbox visualstudiocode 7zip googlechrome notepadplusplus.install firefox -y
+choco install vagrant chefdk virtualbox visualstudiocode 7zip googlechrome notepadplusplus.install firefox git.install tortoisegit -y
 refreshenv
 vagrant plugin install vagrant-winrm
 chef gem install kitchen-all
 vagrant box add mwrock/Windows2016
 vagrant box add mwrock/Windows2012R2
 vagrant box add jacqinthebox/windowsserver2016core
-code --install-extension ms-vscode.powershell
 code --install-extension ms-vscode.powershell
 code --install-extension msazurermtools.azurerm-vscode-tools
 code --install-extension HookyQR.beautify
