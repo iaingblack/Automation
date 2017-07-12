698ace58772dfda35c5f38b6b0ac51bd5f1872bc
iaingblack <iaingblack@gmail.com>
2017-07-12 11:21:01 +0100

    Added vmware workstation and chef for vscode

--- ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
+++ ServerSetupScripts/Windows/Chocolatey/Vagrant.bat
@@ -1,5 +1,5 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install googlechrome 7zip notepadplusplus.install firefox git.install tortoisegit vagrant chefdk virtualbox visualstudiocode -y
+choco install googlechrome 7zip notepadplusplus.install firefox git.install tortoisegit vagrant chefdk virtualbox visualstudiocode vmwareworkstation -y
 refreshenv
 vagrant plugin install vagrant-winrm
 vagrant box add mwrock/Windows2016 --provider virtualbox
@@ -13,6 +13,7 @@ code --install-extension ms-vscode.PeterJausovec.vscode-docker
 code --install-extension alefragnani.project-manager
 code --install-extension Borke.puppet
 code --install-extension mauve.terraform
+code --install-extension Pendrica.chef
 code --install-extension bbenoist.vagrant
 code --list-extensions
 mkdir c:\github
